b45fdd974ffad9cdd4b8606e04be9e9a6d505c94
Altug Cakmakci <altugcakmakci@gmail.com>
2021-11-16 15:07:16 -0500

    Updates

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import PortfolioContainer from "./components/PortfolioContainer";
-import projects from './Projects.js';
-import proficiencies from './Proficiencies.js';
-
-const App = () => <PortfolioContainer projects={projects} proficiencies={proficiencies}/>;
-
-export default App;
